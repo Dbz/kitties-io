0b666f83b4212f369e332f721a70ef5c7d93cbbb
Daniel <BurtDaniel@Gmail.com>
2014-11-25 15:07:20 -0800

    css improvements

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,13 @@
 class Image < ActiveRecord::Base
+  attr_accessor :main_img
+  
   belongs_to :listing
   validates :url, :listing_id, presence: true
   validate :only_main_image
   
   def only_main_image
     if self.main_img == 1
-      image = Image.where({ main_img: 1});
+      image = Image.where({ main_img: 1}).where.not({ id: self.id });
       image.main_img = 0;
       image.save
     end
