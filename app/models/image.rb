afc3cd533aeb8b72c744b26aa1b3765716be4bd0
Daniel <BurtDaniel@Gmail.com>
2014-11-25 12:06:11 -0800

    updated navbar

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,14 @@
 class Image < ActiveRecord::Base
   belongs_to :listing
   validates :url, :listing_id, presence: true
+  validate :only_main_image
+  
+  def only_main_image
+    if self.main_img == 1
+      image = Image.where({ main_img: 1});
+      image.main_img = 0;
+      image.save
+    end
+  end
   
 end
