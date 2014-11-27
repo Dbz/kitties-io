7654db5212ac7069cc2da4a0da96e4deaff16e76
Daniel <BurtDaniel@Gmail.com>
2014-11-27 02:27:09 -0800

    heroku image fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Shop.create(name: "The Animal Auction House", user_id: 1, description: "Buy all 
 
 Listing.create(name: "Ocelot", price: 12, available: 1, shop_id: 1, description: "Buy this furry friend while he's still around! Ocelot pelts are in high demand due to what wonderful pillows they make")
 
-Image.create(listing_id: 1, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/philosophyocelot2.png?X-Amz-Date=20141127T064958Z&X-Amz-Expires=300&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Signature=1f31b4aac170e2af93c0f2d01a4f615b1605b6b5e32414a7ac3c65ca5ceaf840&X-Amz-Credential=ASIAJW3JVW6ZMNPP3XPA/20141127/us-west-1/s3/aws4_request&X-Amz-SignedHeaders=Host&x-amz-security-token=AQoDYXdzEOH//////////wEakAIs8KXPoeWa0uyzaB8jXLNgpM8otvviUexs/oVzH77a0sgutreZJRD%2BDJ6ECuc0eGFTSt6q2pNXPjXW7s8xPJ75stYal4s7Cy3t3SFQyYGUyrPPIulX5r0h1mDshhP46Iy0jNW%2BWvMih49iNJ9M%2BWGZVYNX/ii3KGz8X5kywE5nnTC5MAm9s4tkWHIaN1L0poemjd9QwaCJs6pOlFnfg%2B11XB8RMd2pd00HSYDc/IKHQ%2Bvtv2thHhBym4vueExkxosj%2BXR9lPUmR5Cp36yFPwPNsHufDyIYKwYmUbn2xITWQSH8A1frzo0TdcCrSuFQ1sL9zer0g9sZYhf4Gi30RY%2B/qiSMF2lF7dog8l725XKCsSDBwtmjBQ%3D%3D")
+Image.create(listing_id: 1, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/philosophyocelot2.png")
 Image.create(listing_id: 1, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/crouchingocelot2.png")
 Image.create(listing_id: 1, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/sleepyocelot2.png")
 
