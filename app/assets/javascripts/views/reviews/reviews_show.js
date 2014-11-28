565f7c28291309938f58441d6f6e5bbba69f6651
Daniel <BurtDaniel@Gmail.com>
2014-11-28 01:36:56 -0800

    minor review change

--- app/assets/javascripts/views/reviews/reviews_show.js
+++ app/assets/javascripts/views/reviews/reviews_show.js
@@ -1,6 +1,8 @@
 SpendYourSavings.Views.ReviewShow = Backbone.View.extend({
 
   template: JST['reviews/show'],
+	className: 'review-container',
+	
 	render: function() {
 		var templateCode = this.template({ review: this.model });
 		this.$el.html(templateCode);
