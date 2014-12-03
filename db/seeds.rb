5723c51497c6ae3b71c2bb53d95b1f38e09f4e80
Daniel <BurtDaniel@Gmail.com>
2014-12-03 01:24:31 -0800

    added section to home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.create(username: "matt", password: "mattmatt")
 User.create(username: "Some Dude", password: "somedude")
 
 User.create(username: "Crazy Cat Lady", password: "crazycatlady")
-Image.create(user_id: 5, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/crazycatlady.jpg")
+Image.create(user_id: 5, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/crazycatlady2.jpg")
 
 Shop.create(name: "The Animal Auction House", user_id: 1, description: "Buy all of your exotic cats here!",
 policies: "
@@ -85,7 +85,7 @@ Review.create(
 )
 
 Shop.create(name: "Cats Cats Cats", user_id: 5, description: "I raise cats and then sell them for profit!", policies: "You must pick up your cat. I will NOT ship it!!")
-Image.create(shop_id: 2, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/crazycatlady.jpg")
+Image.create(shop_id: 2, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/crazycatlady2.jpg")
 Listing.create(shop_id: 2, name: "Norwegian Forest Cat", price: 200, available: 1, description: "Its body shape and double coat are what makes this cat so unique. This dense and long-flowing hair provides excellent protection in the winter (and makes it a soft cuddling partner). The Norwegian Forest Cat's well-balanced body structure, equilateral triangle-shaped head and bright emerald green eyes (with a band of gold) also gives it an aura of mystery, while its broad chest and well-developed muscles display the cat's power and strength.
 In addition, the Forest Cat can adapt to the weather and change its coat! During spring, it molts its heavy winter coat and dons a lighter one. In the fall, the cat will again molt and shed its summer coat.")
 Image.create(listing_id: 4, url:"https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/forestcat2optimized.png" , main_img: true)
@@ -292,6 +292,5 @@ Seamed 1x1 ribbed collar with spandex
 
 Concealed seam on cuffs")
 Image.create(listing_id: 14, url: "https://s3-us-west-1.amazonaws.com/spendyoursavingsimages/meowychristmasoptimized.png", main_img: true)
-
 
 
